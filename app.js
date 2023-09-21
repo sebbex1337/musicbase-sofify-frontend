ec080c1117bd7ea01561a6bb833a9ccb60f4aad4
sebbex1337 <sefort@hotmail.dk>
2023-09-21 14:43:46 +0200

    fixed showing of artists for tracks

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
-"use strict";
-
-// ------ Imports ---- //
 import { readArtists, readTracks, readAlbums } from "./http.js";
 import { initViews } from "./view.js";
 
-// load window.eventlistner og hjælpefunktion
 window.addEventListener("load", initApp);
 let artists;
 let tracks;
 let albums;
 
-// hjælpefunktion
 async function initApp() {
-  console.log("Musicbase is running 🎉");
-  artists = await readArtists();
-  console.log(artists);
-  tracks = await readTracks();
-  console.log(tracks);
-  albums = await readAlbums();
-  console.log(albums);
+	console.log("Musicbase is running 🎉");
+	artists = await readArtists();
+	// console.log(artists);
+	tracks = await readTracks();
+	// console.log(tracks);
+	albums = await readAlbums();
+	// console.log(albums);
 
-  displayListArtists(artists);
-  displayListTrack(tracks);
-  displayListAlbum(albums);
+	displayListArtists(artists);
+	displayListTrack(tracks);
+	displayListAlbum(albums);
 
-  initViews();
+	initViews();
 }
 
 // -------------- //
@@ -33,29 +28,25 @@ async function initApp() {
 
 // --- Artist html display --- //
 function displayListArtists(artistslist) {
-  document.querySelector("#artist_table tbody").innerHTML = "";
-  if (artistslist) {
-    for (const artist of artistslist) insertArtists(artist);
-  } else {
-    document
-      .querySelector("#artist_table tbody")
-      .innerHTML("No artists found :(");
-  }
+	document.querySelector("#artist_table tbody").innerHTML = "";
+	if (artistslist) {
+		for (const artist of artistslist) insertArtists(artist);
+	} else {
+		document.querySelector("#artist_table tbody").innerHTML("No artists found :(");
+	}
 }
 
 // --- Artist table hjælpefunktion --- //
 function insertArtists(artist) {
-  const artistHTML = /* HTML */ `
-    <tr>
-      <td>${artist.name}</td>
-      <td>${artist.image}</td>
-      <td>${artist.website}</td>
-    </tr>
-  `;
+	const artistHTML = /* HTML */ `
+		<tr>
+			<td>${artist.name}</td>
+			<td><img src="${artist.image}" /></td>
+			<td>${artist.website}</td>
+		</tr>
+	`;
 
-  document
-    .querySelector("#artist_table tbody")
-    .insertAdjacentHTML("beforeend", artistHTML);
+	document.querySelector("#artist_table tbody").insertAdjacentHTML("beforeend", artistHTML);
 }
 
 // -------------- //
@@ -64,27 +55,33 @@ function insertArtists(artist) {
 
 // --- track html display --- //
 function displayListTrack(trackslist) {
-  document.querySelector("#track_table tbody").innerHTML = "";
-  if (trackslist) {
-    for (const track of trackslist) insertTrack(track);
-  } else {
-    document.querySelector("#track_table tbody").innerHTML("No tracks found");
-  }
+	document.querySelector("#track_table tbody").innerHTML = "";
+	if (trackslist) {
+		for (const track of trackslist) insertTrack(track);
+	} else {
+		document.querySelector("#track_table tbody").innerHTML("No tracks found");
+	}
 }
 
 // --- track display hjælpefunktion --- //
 function insertTrack(track) {
-  const trackHTML = /* HTML */ `
-    <tr>
-      <td>${track.name}</td>
-      <td>${track.artistName}</td>
-      <td>${track.duration}</td>
-    </tr>
-  `;
+	let artistsString = "";
+	if (track.artists.length > 1) {
+		for (const artist of track.artists) {
+			artistsString += `${artist.name}, `;
+		}
+	} else {
+		artistsString = track.artists[0].name;
+	}
+	const trackHTML = /* HTML */ `
+		<tr>
+			<td>${track.name}</td>
+			<td>${artistsString}</td>
+			<td>${track.duration}</td>
+		</tr>
+	`;
 
-  document
-    .querySelector("#track_table tbody")
-    .insertAdjacentHTML("beforeend", trackHTML);
+	document.querySelector("#track_table tbody").insertAdjacentHTML("beforeend", trackHTML);
 }
 
 // -------------- //
@@ -93,26 +90,24 @@ function insertTrack(track) {
 
 // --- album html display --- //
 function displayListAlbum(albumlist) {
-  document.querySelector("#album_table tbody").innerHTML = "";
-  if (albumlist) {
-    for (const album of albumlist) insertAlbum(album);
-  } else {
-    document.querySelector("#album_table tbody").innerHTML("No albums found");
-  }
+	document.querySelector("#album_table tbody").innerHTML = "";
+	if (albumlist) {
+		for (const album of albumlist) insertAlbum(album);
+	} else {
+		document.querySelector("#album_table tbody").innerHTML("No albums found");
+	}
 }
 
 // --- album display hjælpefunktion --- //
 function insertAlbum(album) {
-  const albumHTML = /* HTML */ `
-    <tr>
-      <td>${album.name}</td>
-      <td>${album.releaseDate}</td>
-      <td>${album.image}</td>
-      <td>${album.artistName}</td>
-    </tr>
-  `;
+	const albumHTML = /* HTML */ `
+		<tr>
+			<td>${album.name}</td>
+			<td>${album.releaseDate}</td>
+			<td><img src="${album.image}" /></td>
+			<td>${album.artistName}</td>
+		</tr>
+	`;
 
-  document
-    .querySelector("#album_table tbody")
-    .insertAdjacentHTML("beforeend", albumHTML);
+	document.querySelector("#album_table tbody").insertAdjacentHTML("beforeend", albumHTML);
 }
--- http.js
+++ http.js
@@ -2,23 +2,23 @@ const endpoint = "http://127.0.0.1:3333";
 
 // ----- Read all artists ----- //
 async function readArtists() {
-  const res = await fetch(`${endpoint}/artists`);
-  const artistsData = await res.json();
-  return artistsData;
+	const res = await fetch(`${endpoint}/artists`);
+	const artistsData = await res.json();
+	return artistsData;
 }
 
 // ---- Read all songs ------ //
 async function readTracks() {
-  const res = await fetch(`${endpoint}/tracks`);
-  const tracksData = await res.json();
-  return tracksData;
+	const res = await fetch(`${endpoint}/tracks`);
+	const tracksData = await res.json();
+	return tracksData;
 }
 
 // ----- Read all albums ---- //
 async function readAlbums() {
-  const res = await fetch(`${endpoint}/albums`);
-  const albumsData = await res.json();
-  return albumsData;
+	const res = await fetch(`${endpoint}/albums`);
+	const albumsData = await res.json();
+	return albumsData;
 }
 
 export { readArtists, readTracks, readAlbums };
