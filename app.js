9935fffda23892d73f50a3696ea3168a876de75e
sebbex1337 <sefort@hotmail.dk>
2023-09-23 10:58:46 +0200

    removed consolelogs

--- app.js
+++ app.js
@@ -7,25 +7,22 @@ let tracks;
 let albums;
 
 async function initApp() {
-  console.log("Musicbase is running 🎉");
-  artists = await readArtists();
-  // console.log(artists);
-  tracks = await readTracks();
-  // console.log(tracks);
-  albums = await readAlbums();
-  //   console.log(albums);
+	console.log("Musicbase is running 🎉");
+	artists = await readArtists();
+	tracks = await readTracks();
+	albums = await readAlbums();
 
-  // --- Display functions --- //
-  displayListArtists(artists);
-  displayListTrack(tracks);
-  displayListAlbum(albums);
+	// --- Display functions --- //
+	displayListArtists(artists);
+	displayListTrack(tracks);
+	displayListAlbum(albums);
 
-  // --- Search --- //
-  document.querySelector("#search").addEventListener("keyup", search);
-  document.querySelector("#search").addEventListener("change", search);
+	// --- Search --- //
+	document.querySelector("#search").addEventListener("keyup", search);
+	document.querySelector("#search").addEventListener("change", search);
 
-  // --- Init views --- //
-  initViews();
+	// --- Init views --- //
+	initViews();
 }
 
 // --------------- //
@@ -34,29 +31,25 @@ async function initApp() {
 
 // --- Artist html display --- //
 function displayListArtists(artistslist) {
-  document.querySelector("#artist_table tbody").innerHTML = "";
-  if (artistslist) {
-    for (const artist of artistslist) insertArtists(artist);
-  } else {
-    document
-      .querySelector("#artist_table tbody")
-      .innerHTML("No artists found :(");
-  }
+	document.querySelector("#artist_table tbody").innerHTML = "";
+	if (artistslist) {
+		for (const artist of artistslist) insertArtists(artist);
+	} else {
+		document.querySelector("#artist_table tbody").innerHTML("No artists found :(");
+	}
 }
 
 // --- Artist table hjælpefunktion --- //
 function insertArtists(artist) {
-  const artistHTML = /* HTML */ `
-    <tr>
-      <td>${artist.name}</td>
-      <td><img src="${artist.image}" /></td>
-      <td>${artist.website}</td>
-    </tr>
-  `;
+	const artistHTML = /* HTML */ `
+		<tr>
+			<td>${artist.name}</td>
+			<td><img src="${artist.image}" /></td>
+			<td>${artist.website}</td>
+		</tr>
+	`;
 
-  document
-    .querySelector("#artist_table tbody")
-    .insertAdjacentHTML("beforeend", artistHTML);
+	document.querySelector("#artist_table tbody").insertAdjacentHTML("beforeend", artistHTML);
 }
 
 // -------------- //
@@ -65,35 +58,33 @@ function insertArtists(artist) {
 
 // --- track html display --- //
 function displayListTrack(trackslist) {
-  document.querySelector("#track_table tbody").innerHTML = "";
-  if (trackslist) {
-    for (const track of trackslist) insertTrack(track);
-  } else {
-    document.querySelector("#track_table tbody").innerHTML("No tracks found");
-  }
+	document.querySelector("#track_table tbody").innerHTML = "";
+	if (trackslist) {
+		for (const track of trackslist) insertTrack(track);
+	} else {
+		document.querySelector("#track_table tbody").innerHTML("No tracks found");
+	}
 }
 
 // --- track display hjælpefunktion --- //
 function insertTrack(track) {
-  let artistsString = "";
-  if (track.artists.length > 1) {
-    for (const artist of track.artists) {
-      artistsString += `${artist.name}, `;
-    }
-  } else {
-    artistsString = track.artists[0].name;
-  }
-  const trackHTML = /* HTML */ `
-    <tr>
-      <td>${track.name}</td>
-      <td>${artistsString}</td>
-      <td>${track.duration}</td>
-    </tr>
-  `;
+	let artistsString = "";
+	if (track.artists.length > 1) {
+		for (const artist of track.artists) {
+			artistsString += `${artist.name}, `;
+		}
+	} else {
+		artistsString = track.artists[0].name;
+	}
+	const trackHTML = /* HTML */ `
+		<tr>
+			<td>${track.name}</td>
+			<td>${artistsString}</td>
+			<td>${track.duration}</td>
+		</tr>
+	`;
 
-  document
-    .querySelector("#track_table tbody")
-    .insertAdjacentHTML("beforeend", trackHTML);
+	document.querySelector("#track_table tbody").insertAdjacentHTML("beforeend", trackHTML);
 }
 
 // -------------- //
@@ -102,36 +93,34 @@ function insertTrack(track) {
 
 // --- album html display --- //
 function displayListAlbum(albumlist) {
-  document.querySelector("#album_table tbody").innerHTML = "";
-  if (albumlist) {
-    for (const album of albumlist) insertAlbum(album);
-  } else {
-    document.querySelector("#album_table tbody").innerHTML("No albums found");
-  }
+	document.querySelector("#album_table tbody").innerHTML = "";
+	if (albumlist) {
+		for (const album of albumlist) insertAlbum(album);
+	} else {
+		document.querySelector("#album_table tbody").innerHTML("No albums found");
+	}
 }
 
 // --- album display hjælpefunktion --- //
 function insertAlbum(album) {
-  let artistsString = "";
-  if (album.artists.length > 1) {
-    for (const artist of album.artists) {
-      artistsString += `${artist.name}, `;
-    }
-  } else {
-    artistsString = album.artists[0].name;
-  }
-  const albumHTML = /* HTML */ `
-    <tr>
-      <td>${album.name}</td>
-      <td>${album.releaseDate}</td>
-      <td><img src="${album.image}" /></td>
-      <td>${artistsString}</td>
-    </tr>
-  `;
+	let artistsString = "";
+	if (album.artists.length > 1) {
+		for (const artist of album.artists) {
+			artistsString += `${artist.name}, `;
+		}
+	} else {
+		artistsString = album.artists[0].name;
+	}
+	const albumHTML = /* HTML */ `
+		<tr>
+			<td>${album.name}</td>
+			<td>${album.releaseDate}</td>
+			<td><img src="${album.image}" /></td>
+			<td>${artistsString}</td>
+		</tr>
+	`;
 
-  document
-    .querySelector("#album_table tbody")
-    .insertAdjacentHTML("beforeend", albumHTML);
+	document.querySelector("#album_table tbody").insertAdjacentHTML("beforeend", albumHTML);
 }
 // ---Search function --- //
 
@@ -139,29 +128,25 @@ function insertAlbum(album) {
 // --- ARTIST SEARCH --- //
 // --------------------- //
 function displaySearchArtists(artistslist) {
-  document.querySelector("#artist_search_table tbody").innerHTML = "";
-  if (artistslist) {
-    for (const artist of artistslist) insertSearchArtists(artist);
-  } else {
-    document
-      .querySelector("#artist_search_table tbody")
-      .innerHTML("No artists found :(");
-  }
+	document.querySelector("#artist_search_table tbody").innerHTML = "";
+	if (artistslist) {
+		for (const artist of artistslist) insertSearchArtists(artist);
+	} else {
+		document.querySelector("#artist_search_table tbody").innerHTML("No artists found :(");
+	}
 }
 
 // --- Artist table hjælpefunktion --- //
 function insertSearchArtists(artist) {
-  const artistHTML = /* HTML */ `
-    <tr>
-      <td>${artist.name}</td>
-      <td><img src="${artist.image}" /></td>
-      <td>${artist.website}</td>
-    </tr>
-  `;
+	const artistHTML = /* HTML */ `
+		<tr>
+			<td>${artist.name}</td>
+			<td><img src="${artist.image}" /></td>
+			<td>${artist.website}</td>
+		</tr>
+	`;
 
-  document
-    .querySelector("#artist_search_table tbody")
-    .insertAdjacentHTML("beforeend", artistHTML);
+	document.querySelector("#artist_search_table tbody").insertAdjacentHTML("beforeend", artistHTML);
 }
 
 // --------------------- //
@@ -169,29 +154,24 @@ function insertSearchArtists(artist) {
 // --------------------- //
 
 function displaySearchTrack(trackslist) {
-  document.querySelector("#track_search_table tbody").innerHTML = "";
-  if (trackslist) {
-    for (const track of trackslist) insertSearchTrack(track);
-  } else {
-    document
-      .querySelector("#track_search_table tbody")
-      .innerHTML("No tracks found");
-  }
+	document.querySelector("#track_search_table tbody").innerHTML = "";
+	if (trackslist) {
+		for (const track of trackslist) insertSearchTrack(track);
+	} else {
+		document.querySelector("#track_search_table tbody").innerHTML("No tracks found");
+	}
 }
 
 // --- track display hjælpefunktion --- //
 function insertSearchTrack(track) {
+	const trackHTML = /* HTML */ `
+		<tr>
+			<td>${track.name}</td>
+			<td>${track.duration}</td>
+		</tr>
+	`;
 
-  const trackHTML = /* HTML */ `
-    <tr>
-      <td>${track.name}</td>
-      <td>${track.duration}</td>
-    </tr>
-  `;
-
-  document
-    .querySelector("#track_search_table tbody")
-    .insertAdjacentHTML("beforeend", trackHTML);
+	document.querySelector("#track_search_table tbody").insertAdjacentHTML("beforeend", trackHTML);
 }
 
 // --------------------- //
@@ -200,41 +180,35 @@ function insertSearchTrack(track) {
 
 // --- album html display --- //
 function displaySearchAlbum(listOfAlbums) {
-  document.querySelector("#album_search_table tbody").innerHTML = "";
-  if (listOfAlbums) {
-    for (const album of listOfAlbums) insertSearchAlbum(album);
-  } else {
-    document
-      .querySelector("#album_search_table tbody")
-      .innerHTML("No albums found");
-  }
+	document.querySelector("#album_search_table tbody").innerHTML = "";
+	if (listOfAlbums) {
+		for (const album of listOfAlbums) insertSearchAlbum(album);
+	} else {
+		document.querySelector("#album_search_table tbody").innerHTML("No albums found");
+	}
 }
 
 // --- album display hjælpefunktion --- //
 function insertSearchAlbum(album) {
-  const albumHTML = /* HTML */ `
-    <tr>
-      <td>${album.name}</td>
-      <td>${album.releaseDate}</td>
-      <td><img src="${album.image}" /></td>
-    </tr>
-  `;
+	const albumHTML = /* HTML */ `
+		<tr>
+			<td>${album.name}</td>
+			<td>${album.releaseDate}</td>
+			<td><img src="${album.image}" /></td>
+		</tr>
+	`;
 
-  document
-    .querySelector("#album_search_table tbody")
-    .insertAdjacentHTML("beforeend", albumHTML);
+	document.querySelector("#album_search_table tbody").insertAdjacentHTML("beforeend", albumHTML);
 }
 
 // --- Search function --- //
 async function search(event) {
-  const search = event.target.value;
-  console.log(event.target.value);
-  const results = await getAllSearched(search);
-  console.log(results);
-  const searchedAlbums = results.albums;
-  displaySearchAlbum(searchedAlbums);
-  const searchedTracks = results.tracks;
-  displaySearchTrack(searchedTracks);
-  const searchedArtists = results.artists;
-  displaySearchArtists(searchedArtists);
+	const search = event.target.value;
+	const results = await getAllSearched(search);
+	const searchedAlbums = results.albums;
+	displaySearchAlbum(searchedAlbums);
+	const searchedTracks = results.tracks;
+	displaySearchTrack(searchedTracks);
+	const searchedArtists = results.artists;
+	displaySearchArtists(searchedArtists);
 }
