bd8b24ab7784b23ac6ae4efee02b0095e39baf32
sebbex1337 <sefort@hotmail.dk>
2023-10-13 12:58:58 +0200

    removed console log

--- controller/tracks-controller/deleteTrack.js
+++ controller/tracks-controller/deleteTrack.js
@@ -2,7 +2,6 @@ import { deleteTrack } from "../http.js";
 import { displayUpdatedLists } from "../../app.js";
 
 async function deleteTrackClicked(track) {
-    console.log(track);
     const response = await deleteTrack(track);
     if (response) {
         await displayUpdatedLists();
